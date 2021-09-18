0b9b6f6984f453a71dfb99b2f7feef23595576b7
elegent-codes <yaser.amini67@gmail.com>
2021-09-18 16:45:03 +0430

    capture text from form

--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
 from .models import TodoList
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 
 def index(request):
     # Retrieve objects from db - By 'id' order
     todo_items = TodoList.objects.order_by('id')
-    context = {'todo_items': todo_items}
+    form = TodoListForm()
+    context = {'todo_items': todo_items, 'form': form}
     return render(request, 'base/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo = TodoList(text=request.POST['text'])
+        new_todo.save()
+
+    return redirect('index')
